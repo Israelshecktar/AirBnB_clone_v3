e3947b7738dfe0dcaf7fb04b752d7cf7940f18f0
Israelshecktar <iamshecktar1996@gmail.com>
2024-02-23 21:42:35 +0100

    404 status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-"""This is the module for the Flask application"""
-from flask import Flask
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Closes the storage on app exit"""
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") or "0.0.0.0"
-    port = getenv("HBNB_API_PORT") or "5000"
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
