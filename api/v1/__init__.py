15635d04bf7a8d5bd12914b1208f9e0cf012b388
Israelshecktar <iamshecktar1996@gmail.com>
2024-02-24 07:05:07 +0100

    updated flask file

--- api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""This is the package initializer for the api/v1 module"""
-from flask import Blueprint
-from api.v1.views import index
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
